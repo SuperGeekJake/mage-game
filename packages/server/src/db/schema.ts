a37fcc2219c3a0bfd63937c9483bb2ca8ba289e9
Jacob Williams <supergeekjake@gmail.com>
2025-07-30 10:21:23 -0700

    feat: Add user relations to ORM

--- packages/server/src/db/schema.ts
+++ packages/server/src/db/schema.ts
@@ -1,18 +1,30 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { relations } from "drizzle-orm";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
-export const users = sqliteTable('users', {
-  id: text('id').notNull().primaryKey(),
-  name: text('name'),
-  email: text('email').notNull(),
-  emailVerified: integer('emailVerified', { mode: 'timestamp_ms' }),
-  image: text('image'),
+export const users = sqliteTable("users", {
+  id: text("id").notNull().primaryKey(),
+  name: text("name"),
+  email: text("email").notNull(),
+  emailVerified: integer("emailVerified", { mode: "timestamp_ms" }),
+  image: text("image"),
 });
 
-export const sessions = sqliteTable('sessions', {
-  id: text('id').notNull().primaryKey(),
-  sessionToken: text('sessionToken').notNull(),
-  userId: text('userId')
+export const usersRelations = relations(users, ({ many }) => ({
+  sessions: many(sessions),
+}));
+
+export const sessions = sqliteTable("sessions", {
+  id: text("id").notNull().primaryKey(),
+  sessionToken: text("sessionToken").notNull(),
+  userId: text("userId")
     .notNull()
-    .references(() => users.id, { onDelete: 'cascade' }),
-  expires: integer('expires', { mode: 'timestamp_ms' }).notNull(),
+    .references(() => users.id, { onDelete: "cascade" }),
+  expires: integer("expires", { mode: "timestamp_ms" }).notNull(),
 });
+
+export const sessionsRelations = relations(sessions, ({ one }) => ({
+  user: one(users, {
+    fields: [sessions.userId],
+    references: [users.id],
+  }),
+}));

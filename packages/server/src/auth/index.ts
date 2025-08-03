d7ce3b0838f656960ac3a8d3790763a5d86a76a2
Jacob Williams <supergeekjake@gmail.com>
2025-08-02 19:06:50 -0700

    wip: Begin switch to better-auth

--- packages/server/src/auth/index.ts
+++ packages/server/src/auth/index.ts
@@ -1,26 +1,43 @@
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import Nodemailer from "@auth/core/providers/nodemailer";
-import type { AuthConfig } from "@auth/core";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { magicLink } from "better-auth/plugins";
 import z from "zod";
+import { Resend } from "resend";
 
-import { db } from "../db/index.js";
+import { db } from "~/db/index.js";
 
 const ENV = z
   .object({
-    EMAIL_SERVER: z.string(),
+    AUTH_SECRET: z.string(),
+    BASE_URL: z.string(),
     EMAIL_FROM: z.string(),
+    RESEND_API_KEY: z.string(),
   })
   .parse({
-    EMAIL_SERVER: process.env.EMAIL_SERVER,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    BASE_URL: process.env.BASE_URL,
     EMAIL_FROM: process.env.EMAIL_FROM,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
   });
 
-export const authConfig: AuthConfig = {
-  adapter: DrizzleAdapter(db),
-  providers: [
-    Nodemailer({
-      server: ENV.EMAIL_SERVER,
-      from: ENV.EMAIL_FROM,
+const resend = new Resend(ENV.RESEND_API_KEY);
+
+export const auth = betterAuth({
+  secret: ENV.AUTH_SECRET,
+  baseURL: ENV.BASE_URL,
+  database: drizzleAdapter(db, {
+    provider: "sqlite",
+  }),
+  plugins: [
+    magicLink({
+      sendMagicLink: async ({ email, token, url }, request) => {
+        await resend.emails.send({
+          from: ENV.EMAIL_FROM,
+          to: email,
+          subject: "Mage Game: Verify Your Email Address",
+          html: `The following link will verify your email address and sign you up to play <em>Mage</em>.\n\n<a href="${url}">${url}</a>`,
+        });
+      },
     }),
   ],
-};
+});
